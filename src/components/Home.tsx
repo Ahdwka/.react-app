e94bc2483714b9dff09e7e4b59102785baa29e14
Ahdwka <ahed.wka4g@gmail.com>
2025-03-15 01:46:52 +0300

    Added new feature to handle user login

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,9 +16,11 @@ export function Home() {
       <h1 className="text-4xl font-bold text-gray-800 mb-2 text-center">
         IUST Lecture Materials
       </h1>
-      <div className="text-2xl font-bold text-gray-800 mb-8 text-center">
+      <div className="text-2xl font-bold text-gray-800 mb-2 text-center">
         BY AHED ALKHALAf
       </div>
+      <div className="text-lg font-bold text-gray-800 mb-10 text-center">
+...يتم تحميل تتمة المقررات      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {lectures.map((lecture) => (
           <LectureCard
